7d09ab9408fe9d129f059ab1db98230ccf59ea0f
Steve Hoang <stevehoang@Steves-Mac-mini.zyxel.com>
2023-11-21 11:24:13 -0500

    updated quantile_normalize(), added tests, and initialized poetry

--- omiclass/utils.py
+++ omiclass/utils.py
@@ -2,20 +2,24 @@ import pandas as pd
 import numpy as np
 
 
-def quantile_normalize(df, axis = 0):
+def quantile_normalize(df, axis=0):
     """
-    Perform quantile normalization on a pandas DataFrame.
+    Perform quantile normalization on a pandas DataFrame along the specified axis.
 
     Quantile normalization is a technique used to transform the data so that
-    the distribution of the values for each sample in a DataFrame is the same.
+    the distribution of the values for each row or column in a DataFrame is the same.
     This is particularly useful in gene expression data analysis, ensuring
-    that the distribution of expression levels is consistent across samples.
+    that the distribution of expression levels is consistent across samples or features.
 
     Parameters
     ----------
     df : pandas.DataFrame
-        A DataFrame where each column is a sample and each row is a feature.
+        A DataFrame where typically each column is a sample and each row is a feature.
         The DataFrame should contain numeric values only.
+    axis : int, default 0
+        The axis along which to normalize the DataFrame.
+        - If 0, normalize each column independently.
+        - If 1, normalize each row independently.
 
     Returns
     -------
@@ -24,29 +28,31 @@ def quantile_normalize(df, axis = 0):
         quantile normalized. The returned DataFrame retains the original
         index and column names.
 
-    Example
-    -------
+    Raises
+    ------
+    ValueError
+        If the `axis` argument is not 0 or 1, or if the DataFrame contains non-numeric data.
+
+    Examples
+    --------
     import pandas as pd
     data = pd.DataFrame({
-    ...  'Sample1': [1, 2, 3],
-    ...  'Sample2': [2, 3, 4]
-    ... })
+        'Sample1': [1, 2, 3],
+        'Sample2': [2, 4, 3]
+    })
     normalized_data = quantile_normalize(data)
     print(normalized_data)
 
     Notes
     -----
-    - Ensure that the input DataFrame contains no missing values or non-numeric
+    - The function does not handle missing values or non-numeric data types.
+      Ensure that the input DataFrame contains no missing values or non-numeric
       data types, as these may lead to errors or unexpected behavior.
-    - This function is designed for batch processing of samples. Individual
-      sample normalization may require a different approach.
     - The normalization process does not distinguish between zeros and missing
       values; it treats all entries equally.
 
     See Also
     --------
-    Pandas Documentation: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.html
-    NumPy Documentation: https://numpy.org/doc/stable/reference/generated/numpy.sort.html
     Wikipedia: https://en.wikipedia.org/wiki/Quantile_normalization
     """
 
@@ -60,10 +66,20 @@ def quantile_normalize(df, axis = 0):
     else:
         raise ValueError("axis must be 0 or 1")
 
-    # sort data frame
-    df_sorted = pd.DataFrame(np.sort(df.values, axis=0), index=df.index, columns=df.columns)
-    df_mean = df_sorted.mean(axis=1)
+    # ensure that all values in df are numeric
+    if not df.map(lambda x: isinstance(x, (int, float)) and not pd.isna(x)).all().all():
+        raise ValueError("All values in df must be either int or float. NaN values are not allowed.")
+
+    # generate distribution from sample means
+    df_sorted = pd.DataFrame(np.sort(df.values, axis=a1), index=df.index, columns=df.columns)
+    df_mean = df_sorted.mean(axis=a2)
+    df_mean = df_mean.sort_values(ascending=True)
     df_mean.index = np.arange(1, len(df_mean) + 1)
-    df_rank = df.rank(method='min').stack().astype(int)
-    df_normalized = df_mean[df_rank].unstack()
+
+    # calculate the ranks of the original values
+    df_rank = df.rank(axis=a1, ascending=True)
+
+    # replace the original values with values from the distribution of means
+    df_normalized = df_rank.map(lambda x: df_mean[x])
+
     return df_normalized
---
+++ tests/test_quantile_normalize.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+import pytest
+from omiclass.utils import quantile_normalize
+
+
+def test_basic_functionality():
+    data = pd.DataFrame({
+        'Sample1': [2, 1, 3],
+        'Sample2': [4, 3, 2]
+    })
+    expected = pd.DataFrame({
+        'Sample1': [2.5, 1.5, 3.5],
+        'Sample2': [3.5, 2.5, 1.5]
+    })
+    result = quantile_normalize(data, axis=0)
+    pd.testing.assert_frame_equal(result, expected)
+
+
+def test_axis_parameter():
+    # Create a test case for axis = 1
+    data = pd.DataFrame({
+        'Sample1': [1, 2, 6],
+        'Sample2': [2, 3, 7]
+    })
+    expected = pd.DataFrame({
+        'Sample1': [3.0, 3.0, 3.0],
+        'Sample2': [4.0, 4.0, 4.0]
+    })
+    result = quantile_normalize(data, axis=1)
+    pd.testing.assert_frame_equal(result, expected)
+
+
+def test_non_numeric_data():
+    data = pd.DataFrame({
+        'Sample1': [1, 'a', 3],
+        'Sample2': [2, 3, 4]
+    })
+    with pytest.raises(ValueError):
+        quantile_normalize(data)
+
+
+def test_missing_values():
+    data = pd.DataFrame({
+        'Sample1': [1, np.nan, 3],
+        'Sample2': [2, 3, 4]
+    })
+    with pytest.raises(ValueError):
+        quantile_normalize(data)
+
+
+def test_empty_dataframe():
+    data = pd.DataFrame()
+    result = quantile_normalize(data)
+    assert result.empty
